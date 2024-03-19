1ba7807f976974885f9ce5f357a2273a4c7559b7
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-03-19 18:09:25 +0000

    test supabase

--- .github/workflows/post.ts
+++ .github/workflows/post.ts
@@ -1,6 +1,12 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(process.argv[2], process.argv[3])
+console.log(process.argv[2]);
+const supabase = createClient(process.argv[2], process.argv[3]);
 
-supabase.from('test').insert({})
+supabase
+	.from("test")
+	.insert({ nanika:crypto.randomUUID() })
+	.then((data) => {
+		console.log(data);
+	});
