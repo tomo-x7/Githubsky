ae4035cf0c98ae66f7861aab8ab5007ece9bc6e2
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-03-19 18:10:32 +0000

    torimatesuto

--- .github/workflows/post.ts
+++ .github/workflows/post.ts
@@ -6,7 +6,7 @@ const supabase = createClient(process.argv[2], process.argv[3]);
 
 supabase
 	.from("test")
-	.insert({ nanika:crypto.randomUUID() })
+	.insert({ nanika:'crypto.randomUUID()' })
 	.then((data) => {
 		console.log(data);
 	});
