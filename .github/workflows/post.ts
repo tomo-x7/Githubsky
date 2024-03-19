d431622e7eb0424510ca56c2889dc7fecf0264c9
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-03-19 17:52:14 +0000

    supabase test

--- .github/workflows/post.ts
+++ .github/workflows/post.ts
@@ -1,4 +1,6 @@
-for(let i = 0;i < process.argv.length; i++){
-    console.log(`argv[${i}] = ${process.argv[i]}`);
-  }
+import { createClient } from '@supabase/supabase-js'
 
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.argv[2], process.argv[3])
+
+supabase.from('test').insert({})
