a2a1745dcdb680d40f445e16a541d8e72ff77ca6
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-03-20 10:11:10 +0000

    test

--- .github/workflows/main.ts
+++ .github/workflows/main.ts
@@ -1,12 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
-import {
-	type AtpSessionData,
-	type AtpSessionEvent,
-	BskyAgent,
-} from "@atproto/api";
-import { post } from "./bsky";
-import { getUsersList } from "./supabase";
-import { getUserData } from "./github";
+// import { createClient } from "@supabase/supabase-js";
+// import {
+// 	type AtpSessionData,
+// 	type AtpSessionEvent,
+// 	BskyAgent,
+// } from "@atproto/api";
+// import { post } from "./bsky";
+// import { getUsersList } from "./supabase";
+// import { getUserData } from "./github";
 const main = async () => {
 	const testdata=require("./testdata.json")
 	//const userslist = getUsersList(process.argv[2], process.argv[3]);
