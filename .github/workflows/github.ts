8547048e930fb885c322ca2a944bfc375455058c
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-03-30 01:01:42 +0900

    GithubActions側にもdayjsを導入

--- .github/workflows/github.ts
+++ .github/workflows/github.ts
@@ -1,3 +1,12 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+dayjs.locale("ja");
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Tokyo");
+
+
 type apires = {
 	id: string;
 	type: string;
@@ -17,29 +26,27 @@ export type week = {
 };
 export const getUsersGithubData = async (username: string): Promise<{count:number,lastweek:week}> => {
 	const alldata: Array<apires> = [];
-	const lastgetday = new Date();
-	lastgetday.setDate(new Date().getDate() - 7);
+	const lastgetday = dayjs();
+	lastgetday.set('date',lastgetday.get('date')-7)
 	for (let i = 1; i < 100; i++) {
 		const data: Array<apires> = await fetch(
 			`https://api.github.com/users/${username}/events?page=${i}`,
 		).then((data) => data.json());
 		alldata.push(...data);
-		if (!(data[0] && lastgetday < new Date(data[data.length - 1].created_at))) {
+		if (!(data[0] && lastgetday < dayjs(data[data.length - 1].created_at))) {
 			break;
 		}
 	}
 
 	const lastweek: week = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0 };
 	let commitcount = 0;
-	const yesterday = new Date();
-	yesterday.setDate(yesterday.getDate() - 1);
+	const yesterday = dayjs();
+	yesterday.set('date',yesterday.get('date')-1)
 	const pushdata = alldata
 		.map((data) => {
-			const day = new Date(data.created_at);
+			const day =dayjs(data.created_at);
 			if (data.type === "PushEvent" && day > lastgetday) {
-				const jst = day;
-				jst.setHours(day.getHours() + 9);
-				lastweek[jst.getDay()] += data.payload.commits.length;
+				lastweek[day.get('day')] += data.payload.commits.length;
 				if (day > yesterday) {
 					commitcount += data.payload.commits.length;
 				}
