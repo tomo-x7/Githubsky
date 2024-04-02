4323bd29f02e04ff16cf33936739a678bdef59ff
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2024-04-02 23:35:10 +0900

    コミット数が0の時も投稿を行うように変更

--- .github/workflows/main.ts
+++ .github/workflows/main.ts
@@ -1,26 +1,20 @@
 import { post } from "./bsky";
-import {
-	type UserData,
-	getUsersList,
-	supabasesetting,
-	writelog,
-} from "./supabase";
+import { type UserData, getUsersList, supabasesetting, writelog } from "./supabase";
 import { getUsersGithubData, type week } from "./github";
 import { cryptosetting } from "./mycrypto";
 import { getimg } from "./api";
 const main = async () => {
 	supabasesetting(process.argv[2], process.argv[3]);
-	cryptosetting(process.argv[4])
+	cryptosetting(process.argv[4]);
 	const userslist = await getUsersList();
 
 	for (const i in userslist) {
 		try {
-			const userdata: UserData & { count?: number; lastweek?: week } =
-				userslist[i];
+			const userdata: UserData & { count?: number; lastweek?: week } = userslist[i];
 			Object.assign(userdata, await getUsersGithubData(userdata.github_name));
-			if (userdata.count !== undefined && userdata.count !== 0&&userdata.lastweek!==undefined) {
+			if (userdata.count !== undefined && userdata.lastweek !== undefined) {
 				//APIを叩いて画像取得
-				const imgblob = await getimg(userdata.count,userdata.lastweek)
+				const imgblob = await getimg(userdata.count, userdata.lastweek);
 				post(
 					userdata.bsky_password,
 					userdata.github_name,
@@ -28,10 +22,10 @@ const main = async () => {
 					userdata.id,
 					userdata.fail_count,
 					userdata.DID,
-					imgblob||undefined
+					imgblob || undefined,
 				);
 			} else {
-				console.log("nocommit");
+				writelog(`${userdata.DID}:何かおかしい\n${userdata.count}\n${userdata.lastweek}`)
 			}
 		} catch (e) {
 			writelog(e);
