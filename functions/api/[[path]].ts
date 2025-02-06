e028a249bf42cfa72c5696c889f3b85d691ed012
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-02-07 02:27:39 +0900

    test

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -1,14 +1,13 @@
 import { Hono } from "hono";
-import { hc } from "hono/client";
 
 const app = new Hono().basePath("api/");
-const app2=new Hono()
+const app2 = new Hono();
 
-const app2schema=app2.get("/*",(c)=>c.text("app2"))
-const Schema=app.get("/", (c) => {
-	return c.text("Hello Hono!");
-}).route("/app2",app2)
+const Schema = app
+	.get("/", (c) => {
+		return c.text("Hello Hono!");
+	})
+	.route("/app2", app2);
 
 export const onRequest: PagesFunction = (c) => app.fetch(c.request);
-
-const client=hc<typeof app2schema>("")
+export type ServerType = typeof Schema;
---
+++ functions/index.ts
@@ -0,0 +1,5 @@
+import { hc } from "hono/client";
+import type { ServerType } from "./api/[[path]]";
+export function createClient() {
+	return hc<ServerType>("/");
+}
