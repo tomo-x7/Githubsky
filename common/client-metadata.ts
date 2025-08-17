005bd8f6b0bba2a7ffcf732b484adfc16777bfb5
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-08-17 22:09:35 +0900

    scope対応

--- common/client-metadata.ts
+++ common/client-metadata.ts
@@ -1,7 +1,7 @@
 import type { ClientMetadata } from "@atproto/oauth-client-node";
 
 const SERVER_DOMAIN = "githubsky.tomo-x.win" as const;
-export const clientMetadata: ClientMetadata = {
+export const clientMetadata = {
 	// Must be a URL that will be exposing this metadata
 	client_id: `https://${SERVER_DOMAIN}/api/client-metadata.json`,
 	client_name: "githubsky",
@@ -10,7 +10,7 @@ export const clientMetadata: ClientMetadata = {
 	// tos_uri: `https://${SERVER_DOMAIN}/tos`,
 	// policy_uri: `https://${SERVER_DOMAIN}/policy`,
 	redirect_uris: [`https://${SERVER_DOMAIN}/api/callback`],
-	scope: "atproto transition:generic",
+	scope: "atproto repo:app.bsky.feed.post?action=create blob:image/png",
 	grant_types: ["authorization_code", "refresh_token"],
 	response_types: ["code"],
 	application_type: "web",
@@ -18,4 +18,6 @@ export const clientMetadata: ClientMetadata = {
 	dpop_bound_access_tokens: true,
 	jwks_uri: `https://${SERVER_DOMAIN}/api/jwks.json`,
 	token_endpoint_auth_signing_alg: "ES256",
-};
+	subject_type: "public",
+	authorization_signed_response_alg: "RS256",
+} satisfies ClientMetadata;
